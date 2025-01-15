cf18641b589e6e6c9fdf39229611838759b20b3c
Joe Clark <weirdlookingjay@live.com>
2025-01-15 10:36:59 -0500

    (Feature): Created Navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@/components/navbar"
 import Sidebar from "@/components/sidebar"
 
 interface DashboardLayoutProps {
@@ -11,8 +12,9 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
                 <div className="fixed left-0 top-0 hidden lg:block lg:w-[246px] h-full overflow-y-auto">
                     <Sidebar />
                 </div>
-                <div className="lg:pl-[246px]">
+                <div className="lg:pl-[246px] w-full">
                     <div className="mx-auto max-w-screen-2xl h-full">
+                        <Navbar />
                         <main className="h-full py-8 px-6 flex flex-col">
                             {children}
                         </main>
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,4 @@
 import { getCurrent } from "@/features/auth/actions";
-import { UserButton } from "@/features/auth/components/user-button";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
@@ -7,7 +6,7 @@ export default async function Home() {
   if (!user) redirect("/sign-in");
   return (
     <div>
-      <UserButton />
+      Home Page
     </div>
   );
 }
